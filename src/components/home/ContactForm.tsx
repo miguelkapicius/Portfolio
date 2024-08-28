024145191c047c58a865415bdebc41aedbd808b5
Miguel Kapicius <miguelkapicius@gmail.com>
2024-08-28 15:48:26 -0300

    arrumei o form de envio de mensagem

--- src/components/home/ContactForm.tsx
+++ src/components/home/ContactForm.tsx
@@ -1,61 +1,91 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { toast } from "@/components/ui/use-toast"
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+import { Textarea } from "../ui/textarea";
+import { Toaster } from "../ui/toaster";
+import { useForm } from "react-hook-form";
 
 const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+    name: z.string().min(5, {
+        message: "Seu nome completo deve conter no mínimo 5 caracteres.",
+    }),
+    message: z.string().min(5, {
+        message: "Sua mensagem deve conter no mínimo 10 caracteres.",
+    }),
+});
 
 export function ContactForm() {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            name: "",
+            message: "",
+        },
+    });
 
-  function onSubmit (){
-    toast({
-      title: "Sua mensagem foi enviada!",
-    })
-  }
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+        console.log(data);
+        toast({
+            title: "Sua mensagem foi enviada!",
+        });
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  )
+        return;
+    }
+
+    return (
+        <>
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="w-2/3 space-y-6"
+                >
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Nome:</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Nome Completo"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="message"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Mensagem:</FormLabel>
+                                <FormControl>
+                                    <Textarea
+                                        placeholder="Digite sua mensagem aqui..."
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit">Enviar</Button>
+                </form>
+            </Form>
+            {form.handleSubmit(onSubmit) && <Toaster />}
+        </>
+    );
 }
